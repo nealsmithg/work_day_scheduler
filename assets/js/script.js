731700cc2d05ec1d75663da50db6ced80ee6819a
Cornelius Smith <nealsmithg@gmail.com>
2021-09-07 11:03:16 -0400

    finished js for wesite

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,34 @@
 var now = moment();
+var saved = {};
 
 function init() {
-    // Sets interval in variable
-    var timerInterval = setInterval(function() {
+    saved = JSON.parse(localStorage.getItem("scheduleSave"));
+    for (var i = 0; i < Object.keys(saved).length; i++){
+        console.log(Object.keys(saved)[0]);
+        $("ul[data-time="+Object.keys(saved)[i]+"]").children(".description").val(saved[Object.keys(saved)[i]]);
+    }
+    setInterval(function() {
         now = moment();
-        $("#currentDay").text(now.format("MMMM Do YYYY, h:mm:ss a"))
-    }, 100);
+        $("#currentDay").text(now.format("MMMM Do YYYY, h:mm:ss a"));
+        $("ul").each(function(){
+            if ((now.format("H") == $(this).data("time"))) {
+                $(this).children(".description").addClass("present");
+            };
+            if ((now.format("H") > $(this).data("time"))) {
+                $(this).children(".description").addClass("past")
+            };
+            if ((now.format("H") < $(this).data("time"))) {
+                $(this).children(".description").addClass("future")
+            };
+        })
+    }, 500);
   };
 
-  init();
+$(".saveBtn").click(function(){
+    var text = $(this).parent().children(".description").val();
+    var time = $(this).parent().data("time");
+    saved[time] = text;
+    localStorage.setItem("scheduleSave", JSON.stringify(saved));
+});
+
+init();
